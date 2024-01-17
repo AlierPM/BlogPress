b65d9e26d4abe47434ef7ba2a3d735292848da1c
Alier <alierditmaguet@gmail.com>
2024-01-17 13:21:36 +0200

    Request Rspect tests for users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,43 @@
+# spec/requests/users_spec.rb
+
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
-      expect(response).to have_http_status(:success)
+    it 'returns a successful response' do
+      get '/users'
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the index template' do
+      get '/users'
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get '/users'
+      expect(response.body).to include('Find All the Best Authors Here')
     end
   end
 
   describe 'GET /show' do
-    it 'returns http success' do
-      get '/users/show'
-      expect(response).to have_http_status(:success)
+    let(:user) { create(:user) }
+
+    it 'returns a successful response' do
+      get "/users/#{user.id}"
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the show template' do
+      get "/users/#{user.id}"
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get "/users/#{user.id}"
+      # expect(response.body).to include("Details for user #{user.name}")
+      expect(response.body).to include('<h1>The Best BlogPress User</h1>')
+      expect(response.body).to include('<p>Know More About Your Best Author Here</p>')
     end
   end
 end
