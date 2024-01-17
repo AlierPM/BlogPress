0a007fa08d8f9b961ea2daa5877746365423f7de
Alier <alierditmaguet@gmail.com>
2024-01-17 13:21:18 +0200

    Request Rspect tests for posts controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,17 +1,45 @@
+# spec/requests/posts_spec.rb
+
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/posts/index'
-      expect(response).to have_http_status(:success)
+  describe 'GET /users/:user_id/posts/index' do
+    it 'returns a successful response' do
+      user = create(:user) # Assuming you have a user factory
+      get "/users/#{user.id}/posts"
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the index template' do
+      user = create(:user)
+      get "/users/#{user.id}/posts"
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      user = create(:user)
+      get "/users/#{user.id}/posts"
+      expect(response.body).to include("User's Posts Goes Here")
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/posts/show'
-      expect(response).to have_http_status(:success)
+  describe 'GET /users/:user_id/posts/:id/show' do
+    let(:user) { create(:user) }
+    let(:post) { create(:post, author: user) } # Assuming you have a post factory
+
+    it 'returns a successful response' do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the show template' do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response.body).to include('Here is shown details about each post')
     end
   end
 end
