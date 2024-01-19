7db1d7464712323d74cfcd81de7e84d7ae6012d3
Alier <alierditmaguet@gmail.com>
2024-01-19 17:38:00 +0200

    Install Will_paginate and tailwindcss gems

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,32 @@
 class PostsController < ApplicationController
+  before_action :find_user
+  before_action :find_post, only: [:show]
+
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments).paginate(page: params[:page], per_page: 2)
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments)
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:user_id])
+  end
+
+  def find_post
+    find_user
+    @post = @user.posts.find_by(id: params[:id])
+
+    return if @post
+
+    respond_to do |format|
+      format.html { render 'post_not_found' and return }
+      format.js { render 'post_not_found' and return }
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,17 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  # before_action :set_user, only: [:show]
 
   def index
     @users = User.all
   end
 
-  def show; end
-
-  private
-
-  def set_user
+  def show
     @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+
+  def user_posts
+    @user = User.find(params[:id])
+    @posts = @user.posts.includes(:comments, :user)
   end
 end
