c1ff42e377ba3d4cabcf96b2b99a197138f7c742
Alier <alierditmaguet@gmail.com>
2024-01-31 19:38:14 +0200

    Add devise to authenticate users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: %i[index show]
 
   def index
     @users = User.all.includes(:posts, :comments, :likes).order('posts.created_at ASC')
   end
 
   def show
-    @user = current_user
-    # @user = User.includes(:posts).find(params[:user_id]) # Preload associated posts
+    @user = User.includes(:posts).find(params[:id]) # Preload associated posts
+    # @user = User.find(params[:id]).includes(:posts)
+    # pp "user is #{@user}"
     @user_posts = @user ? @user.posts.order('created_at ASC') : []
     @three_most_recent_posts = @user.posts.order('created_at ASC').three_most_recent_posts
     @posts = @user.posts.includes(:comments, :likes).limit(3)
